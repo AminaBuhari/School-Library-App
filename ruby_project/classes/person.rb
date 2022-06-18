a5aafade1b4e04d0533c8685def946750dfce660
buhariaminat <buhariaminat@yahoo.com>
2022-06-18 10:07:22 +0100

    Add relationship

--- ruby_project/classes/person.rb
+++ ruby_project/classes/person.rb
@@ -1,14 +1,15 @@
 require_relative './nameable'
 
 class Person < Nameable
-  attr_reader :id
+  attr_reader :id, :rentals
   attr_accessor :name, :age
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission = true)
     @parent_permission = parent_permission
     @id = Random.rand(1..1000)
     @name = name
     @age = age
+    @rentals = []
     super()
   end
 
--- ruby_project/classes/student.rb
+++ ruby_project/classes/student.rb
@@ -1,13 +1,16 @@
-require './person'
+require_relative './person'
+require_relative './classroom'
 
 class Student < Person
-  attr_accessor :classroom
-
-  def initialize(age, name, parent_permission, classroom)
+  attr_reader :classroom
+ public
+  def student(age, name, parent_permission)
     super(age, name, parent_permission)
     @classroom = classroom
+    @classroom.add_student(self) unless classroom.students.include?(self)
+    
   end
-
+  
   def play_hooky
     '¯\(ツ)/¯'
   end
